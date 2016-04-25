b16463226d7405430fabf94756f4fda96acf9fd2
Junpeng Lao <junpeng.lao@unifr.ch>
2016-04-25 10:19:50 +0200

    Merge pull request from Nayla (#6)
    * Merge branch 'master' into pr/5
    
    # Conflicts:
    #	GUI/imap_gui.m
    
    * fixed

--- GUI/function_find_fixation.m
+++ GUI/function_find_fixation.m
@@ -1,7 +1,8 @@
-function [data,counter,mapType] = function_find_fixation(handles)
+function [data,counter,mapType,check_number_fixations] = function_find_fixation(handles,fix_number)
 data = handles.data; %get the data
 counter = 0;
 mapType = 1;
+check_number_fixations = 1; % this indicates that we have more than one fixation per trial and thus would able to do the estimate and the single trial method
 
 % find the fixation index and and the maximumn number of fixations
 % retrieve the trial column data
@@ -9,14 +10,14 @@ categorical_conditions = handles.predictors_categorical_default;
 
 
 %Transform the trial into numbers
-[C, ic,trial] = unique(handles.data(:,categorical_conditions.idx_categorical_default(2)),'stable');
-
+ [C, ic,trial] = unique(handles.data(:,categorical_conditions.idx_categorical_default(2)),'stable');
+if fix_number~=1
 if(any(isnan(trial)==1)) % if this condition is true, something is wrong with the trial column
-    max_number_fixation = [];
+    max_number_fixation = 1;
     fixation_index = [];
 else
     % find the number of fixation
-    number_fixation = findseq(trial)
+    number_fixation = findseq(trial);
     
     % find the maximum number of fixation
     max_number_fixation = max(number_fixation(:,4));
@@ -30,11 +31,17 @@ else
         fixation_index(number_fixation(j,2):number_fixation(j,3)) = 1:number_fixation(j,4);
     end
 end
+else
+    counter = 1;
+    mapType = 2;
+    max_number_fixation = -1;% -1 is just to distiguish between one fixation or when the user enter a wrong column for trial!
+    fixation_index = ones(1,length(trial));
+end
 
 if isempty(max_number_fixation)==1
     errordlg('Unable to retrieve the number of fixation. Make sure you select the right column for trial (i.e., numerical values).');
     
-else
+elseif max_number_fixation>1
     size_figure = [340,150];
     screensize = get(0,'ScreenSize');
     xpos_1 = ceil((screensize(3)-size_figure(2))/2); % center the figure on the screen horizontally
@@ -99,6 +106,50 @@ else
     %         counter = 0;
     %         return;
     %     end
+    
+else
+   check_number_fixations = 0; % This means we have only one fixation and thus only the estimated method should be used 
+   size_figure = [340,100];
+    screensize = get(0,'ScreenSize');
+    xpos_1 = ceil((screensize(3)-size_figure(2))/2); % center the figure on the screen horizontally
+    ypos_1 = ceil((screensize(4)-size_figure(1))/2); % center the figure on the screen vertically
+    %%
+    %300,200
+    S.fh = figure('units','pixels',...
+        'position',[xpos_1 ypos_1 size_figure(1) size_figure(2)],...
+        'menubar','none',...
+        'name','Fixation Options',...
+        'numbertitle','off',...
+        'resize','off');
+    set(S.fh,'color','white')
+    %movegui(S.fh,'center');
+    % type fixation
+    type_fixation0 = {'Duration Map','Number of Fixation Map'};
+    % create checkboxes
+    hBtnGrp0 = uibuttongroup('Position',[-.01 0 1.02 1],'BackgroundColor', [1 1 1]);
+    %set(hBtnGrp,'color','white');
+    
+    boxhandles0(1) =  uicontrol('Style','Radio', 'Parent',hBtnGrp0, 'HandleVisibility','off',...
+        'Position',[30 60  100 20], 'String',type_fixation0{1}) ;
+    
+    boxhandles0(2) =  uicontrol('Style','Radio', 'Parent',hBtnGrp0, 'HandleVisibility','off',...
+        'Position',[160 60  150 20], 'String',type_fixation0{2}) ;
+    
+    r = {'What type of fixation map would you like?'};
+    S.text_general = uicontrol('style','text','string',r,'position',[10 125 300  15],...
+        'HorizontalAlignment','center','FontWeight','Bold','Backgroundcolor','white');
+ 
+    boxhandles = []; % we go to the case where we use all fixations since we only have one fixation!
+    sz_pshb1 = [70,20];
+    uicontrol('parent',S.fh,'Style','pushbutton','Units','pixel',...
+        'String','Validate','position',[120 20 sz_pshb1(1) sz_pshb1(2)],...
+        'Callback',{@validate_type_fixation,boxhandles,boxhandles0,max_number_fixation,fixation_index});
+
+    uiwait(gcf)
+    
+     
+  
+  
 end
 
 % local callback functions
@@ -109,7 +160,10 @@ end
         
         %retrieve the choice of the uster
         choice_user = find(cell2mat(get(boxhandles,'Value'))==1);
-                %retrieve the choice of the uster
+        if isempty(choice_user)==1 % We are in the case of one fixation
+        choice_user = 3; % we need all data!
+        end
+        %retrieve the choice of the uster
         mapType = find(cell2mat(get(boxhandles0,'Value'))==1);
 
         %delete(S.fh);
@@ -161,6 +215,8 @@ end
                 if ishandle(S.fh)
                     delete(S.fh);
                 end
+                
+                                
         end
         
         % local callback functions
