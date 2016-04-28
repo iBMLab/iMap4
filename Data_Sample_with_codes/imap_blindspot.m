5a1717a2b31e725bbe71515d7bd6d78a5bab5d6d
Junpeng Lao <junpeng.lao@unifr.ch>
2016-04-28 12:00:38 +0200

    MInor changes
    * Updated installation package
    * example code tide up

--- Data_Sample_with_codes/imap_blindspot.m
+++ Data_Sample_with_codes/imap_blindspot.m
@@ -1,7 +1,8 @@
 %% Load data
 clear all
 clc
-filename=dir('data*.mat');
+cd ('Data_sample_blindspot')
+filename = dir('data*.mat');
 % create condition table
 sbj      = repmat([1:30],1,4)';
 group    = repmat([ones(1,15) ones(1,15)*2],1,4)';
@@ -86,134 +87,19 @@ subplot(1,2,2)
 masktmp=squeeze(mean(fixmapMat2,1))>.01;
 imagesc(masktmp);axis('equal','off')
 %% imapLMM
-tic
-opt.singlepredi=1;
-[LMMmap,lmexample]=imapLMM(fixmapMat2,Tbl,masktmp,opt, ...
-   'PixelIntensity ~ group + blinkspot +  group:blinkspot + (1|sbj)','DummyVarCoding','effect');
-toc
-%% imapLMM_result
-%% plot model fitting
-close all
-opt1.type='model';
-% perform contrast
-[StatMap]=imapLMMcontrast(LMMmap,opt1);
-% output figure;
-imapLMMdisplay(StatMap,0)
-%% plot fixed effec(anova result)
+% tic
+% opt.singlepredi=1;
+% [LMMmap,lmexample]=imapLMM(fixmapMat2,Tbl,masktmp,opt, ...
+%    'PixelIntensity ~ group + blinkspot +  group:blinkspot + (1|sbj)','DummyVarCoding','effect');
+% toc
+% %% imapLMM_result
+% %% plot model fitting
 % close all
-opt=struct;% clear structure
-mccopt=struct;
-opt.type='fixed';
-opt.alpha=.05;
-mccopt.methods='bootstrap';
-mccopt.bootgroup={'groupST'};
-% mccopt.methods='FDR';
-mccopt.nboot=1000;
-% mccopt.permute=1;
-mccopt.bootopt=1;
-% mccopt.sigma=smoothingpic*scale;
-mccopt.tfce=0;
-
-% perform contrast
-[StatMap]=imapLMMcontrast(LMMmap,opt);
-[StatMap_c]=imapLMMmcc(StatMap,LMMmap,mccopt,fixmapMat2ST);
-% output figure;
-imapLMMdisplay(StatMap_c,1)
-%% Compute linear contrast (reproduce figure 2 as in the orignial paper)
-% close all
-opt=struct;% clear structure
-mccopt=opt;
-opt.type='predictor beta';
-opt.alpha=.05;
-opt.c={[-1 0 0 0 1 0 0 0];[0 -1 0 0 0 1 0 0];[0 0 -1 0 0 0 1 0];[0 0 0 -1 0 0 0 1];[1 0 0 -1 0 0 0 0];[0 0 0 0 1 0 0 -1]};
-opt.name={'WC-EA NV';'WC-EA 2dg';'WC-EA 5dg';'WC-EA 8dg';'WC NV-8dg'; 'EA NV-8dg'};
-% opt.c=limo_OrthogContrasts([3,2]);
-% opt.name={'Spotlight';'Position';'Interaction'};
-% opt.h={[0.005],[0.005],[0.005],[0.005],[0.005],[0.005]};
-% opt.onetail='>';
-% mccopt.methods='Randomfield';
-mccopt.methods='bootstrap';
-mccopt.bootgroup={'groupST'};
-% mccopt.methods='FDR';
-mccopt.nboot=1000;
-% mccopt.permute=1;
-mccopt.bootopt=1;
-% mccopt.sigma=smoothingpic*scale;
-mccopt.tfce=0;
-
-% perform contrast
-[StatMap]=imapLMMcontrast(LMMmap,opt);
-[StatMap_c]=imapLMMmcc(StatMap,LMMmap,mccopt,fixmapMat2ST);
-
-% output figure;
-imapLMMdisplay(StatMap_c,1)
-%% Single predictor (above chance fixate)
-opt=struct;% clear structure
-mccopt=opt;
-opt.type='predictor beta';
-opt.alpha=.05;
-opt.c={[1 0 0 0 0 0 0 0];[0 1 0 0 0 0 0 0];[0 0 1 0 0 0 0 0];[0 0 0 1 0 0 0 0];[0 0 0 0 1 0 0 0];[0 0 0 0 0 1 0 0];[0 0 0 0 0 0 1 0];[0 0 0 0 0 0 0 1]};
-opt.name={'EA-NV';'EA-2deg';'EA-5deg';'EA-8deg';'WC-NV';'WC-2deg';'WC-5deg';'WC-8deg'};
-h0=mean(fixmapMat2ST(repmat(masktmpST,[size(fixmapMat2ST,1),1,1])==1));
-opt.h={h0,h0,h0,h0,h0,h0,h0,h0};
-% opt.c=limo_OrthogContrasts([3,2]);
-% opt.name={'Spotlight';'Position';'Interaction'};
-% opt.h={[0.005],[0.005],[0.005],[0.005],[0.005],[0.005]};
-opt.onetail='>';
-% mccopt.methods='Randomfield';
-mccopt.methods='bootstrap';
-mccopt.bootgroup={'groupST'};
-% mccopt.methods='FDR';
-mccopt.nboot=1000;
-% mccopt.permute=1;
-mccopt.bootopt=1;
-% mccopt.sigma=smoothingpic*scale;
-mccopt.tfce=0;
-
-% perform contrast
-[StatMap]=imapLMMcontrast(LMMmap,opt);
-[StatMap_c]=imapLMMmcc(StatMap,LMMmap,mccopt,fixmapMat2ST);
-
-% output figure;
-imapLMMdisplay(StatMap_c,1,[],'parula')
-%% bootCI for fixed effect beta
-% size(StatMap_c.betaCI)
-% size(StatMap_c.resampMat.resBeta)
-cmax=max(StatMap_c.betaCI(:));
-cmin=min(StatMap_c.betaCI(:));
-alpha=.05;
-[nboot,Nc,xSize,ySize]=size(StatMap_c.resampMat.resBeta);
-
-for ic=1:Nc
-    betaORI=squeeze(StatMap_c.beta(ic,:,:));
-    figure('Numbertitle','off','Name',StatMap_c.label{ic});
-    betaCI1=squeeze(StatMap_c.betaCI(ic,1,:,:));
-    betaCI2=squeeze(StatMap_c.betaCI(ic,2,:,:));
-    subplot(2,3,1)
-    imagesc(betaCI1,[cmin cmax]);axis('equal','off')
-    title('lower CI')
-    subplot(2,3,4)
-    imagesc(betaCI2,[cmin cmax]);axis('equal','off')
-    title('upper CI')
-    % compute Bootstrap CI
-    betatmp=squeeze(StatMap_c.resampMat.resBeta(:,ic,:,:));
-    betasort=sort(betatmp(:,:),1);
-    betaCI1b=reshape(betasort(nboot*alpha,:),[xSize,ySize])+betaORI;
-    betaCI2b=reshape(betasort(nboot*(1-alpha),:),[xSize,ySize])+betaORI;
-    subplot(2,3,2)
-    imagesc(betaCI1b,[cmin cmax]);axis('equal','off')
-    title('lower CI (bs)')
-    subplot(2,3,5)
-    imagesc(betaCI2b,[cmin cmax]);axis('equal','off')
-    title('upper CI (bs)')
-
-    subplot(2,3,3)
-    imagesc(betaCI1b-betaCI1);colorbar;axis('equal','off')
-    title('diff CI (bs-pm)')
-    subplot(2,3,6)
-    imagesc(betaCI2b-betaCI2);colorbar;axis('equal','off')
-    title('diff CI (bs-pm)')
-end
+% opt1.type='model';
+% % perform contrast
+% [StatMap]=imapLMMcontrast(LMMmap,opt1);
+% % output figure;
+% imapLMMdisplay(StatMap,0)
 %% Single trial
 Trialall=sum(Ntall);
 rawmapMatST = zeros(Trialall,ySize, xSize);
@@ -270,12 +156,12 @@ TblST.groupST(TblST.groupST=='2')='EA';
 TblST.groupST=nominal(TblST.groupST);
 
 %% rescale
-scale=150/mean([xSize,ySize]);
-[ySize2,xSize2]=size(imresize(ones(ySize,xSize),scale,'nearest'));
-fixmapMat2ST=zeros(size(TblST,1),ySize2,xSize2);
-for it=1:size(TblST,1)
-    fixmapMat2ST(it,:,:)=imresize(squeeze(fixmapMatST(it,:,:)),scale,'nearest');
-end
+% scale=150/mean([xSize,ySize]);
+% [ySize2,xSize2]=size(imresize(ones(ySize,xSize),scale,'nearest'));
+% fixmapMat2ST=zeros(size(TblST,1),ySize2,xSize2);
+% for it=1:size(TblST,1)
+%     fixmapMat2ST(it,:,:)=imresize(squeeze(fixmapMatST(it,:,:)),scale,'nearest');
+% end
 %% mean map
 figure('NumberTitle','off','Name','Mean fixation bias');
 race=unique(TblST.groupST);
@@ -284,7 +170,7 @@ ii=0;
 for ig=1:length(race)
     for ipp=1:length(condi)
         indxtmp=TblST.groupST==race(ig) & TblST.blinkspotST==condi(ipp);
-        TempMap=squeeze(mean(fixmapMat2ST(indxtmp,:,:),1));
+        TempMap=squeeze(mean(fixmapMatST(indxtmp,:,:),1));
         % subplot(2,4,(ig-1)*4+ipp)
         subplot(4,2,ipp*2-(2-ig))
         ii=ii+1;betanew(ii,:,:)=TempMap;
@@ -296,9 +182,9 @@ end
 %
 figure('NumberTitle','off','Name','all fixation bias');
 subplot(1,2,1)
-imagesc(squeeze(mean(fixmapMat2ST,1)));axis('equal','off')
+imagesc(squeeze(mean(fixmapMatST,1)));axis('equal','off')
 subplot(1,2,2)
-masktmpST=squeeze(mean(fixmapMat2ST,1))>.0045;
+masktmpST=squeeze(mean(fixmapMatST,1))>.0045;
 imagesc(masktmpST);axis('equal','off')
 
 %% imapLMM
@@ -307,6 +193,133 @@ opt.singlepredi=1;
 %opt.parallelname='grid2';
 % [LMMmap,lmexample]=imapLMM(fixmapMat2,Tbl,masktmp,opt, ...
 %    'PixelIntensity ~ group + blinkspot +  group:blinkspot + (stDur|sbj)','DummyVarCoding','effect');
-[LMMmap,lmexample]=imapLMM(fixmapMat2ST,TblST,masktmpST,opt, ...
+[LMMmap,lmexample]=imapLMM(fixmapMatST,TblST,masktmpST,opt, ...
    'PixelIntensity ~ groupST + blinkspotST +  groupST:blinkspotST + (1|sbjST)','DummyVarCoding','effect');
 toc
+%% plot fixed effec(anova result)
+% close all
+opt=struct;% clear structure
+mccopt=struct;
+opt.type='fixed';
+opt.alpha=.05;
+mccopt.methods='bootstrap';
+mccopt.bootgroup={'groupST'};
+% mccopt.methods='FDR';
+mccopt.nboot=1000;
+% mccopt.permute=1;
+mccopt.bootopt=1;
+% mccopt.sigma=smoothingpic*scale;
+mccopt.tfce=0;
+
+% perform contrast
+[StatMap]=imapLMMcontrast(LMMmap,opt);
+[StatMap_c]=imapLMMmcc(StatMap,LMMmap,mccopt,fixmapMatST);
+% output figure;
+imapLMMdisplay(StatMap_c,1)
+%% Compute linear contrast (reproduce figure 2 as in the orignial paper)
+% close all
+opt=struct;% clear structure
+mccopt=opt;
+opt.type='predictor beta';
+opt.alpha=.05;
+opt.c={[-1 0 0 0 1 0 0 0]; ...
+       [0 -1 0 0 0 1 0 0]; ...
+       [0 0 -1 0 0 0 1 0]; ...
+       [0 0 0 -1 0 0 0 1]; ...
+       [1 0 0 -1 0 0 0 0]; ...
+       [0 0 0 0 1 0 0 -1]};
+opt.name={'WC-EA NV';'WC-EA 2dg';'WC-EA 5dg';'WC-EA 8dg';'WC NV-8dg'; 'EA NV-8dg'};
+% opt.c=limo_OrthogContrasts([3,2]);
+% opt.name={'Spotlight';'Position';'Interaction'};
+% opt.h={[0.005],[0.005],[0.005],[0.005],[0.005],[0.005]};
+% opt.onetail='>';
+% mccopt.methods='Randomfield';
+mccopt.methods='bootstrap';
+mccopt.bootgroup={'groupST'};
+% mccopt.methods='FDR';
+mccopt.nboot=1000;
+% mccopt.permute=1;
+mccopt.bootopt=1;
+% mccopt.sigma=smoothingpic*scale;
+mccopt.tfce=0;
+
+% perform contrast
+[StatMap]=imapLMMcontrast(LMMmap,opt);
+[StatMap_c]=imapLMMmcc(StatMap,LMMmap,mccopt,fixmapMatST);
+
+% output figure;
+imapLMMdisplay(StatMap_c,1)
+%% Single predictor (above chance fixate)
+opt=struct;% clear structure
+mccopt=opt;
+opt.type='predictor beta';
+opt.alpha=.05;
+opt.c={[1 0 0 0 0 0 0 0]; ...
+       [0 1 0 0 0 0 0 0]; ...
+       [0 0 1 0 0 0 0 0]; ...
+       [0 0 0 1 0 0 0 0]; ...
+       [0 0 0 0 1 0 0 0]; ...
+       [0 0 0 0 0 1 0 0]; ...
+       [0 0 0 0 0 0 1 0]; ...
+       [0 0 0 0 0 0 0 1]};
+opt.name={'EA-NV';'EA-2deg';'EA-5deg';'EA-8deg';'WC-NV';'WC-2deg';'WC-5deg';'WC-8deg'};
+h0=mean(fixmapMatST(repmat(masktmpST,[size(fixmapMatST,1),1,1])==1));
+opt.h={h0,h0,h0,h0,h0,h0,h0,h0};
+% opt.c=limo_OrthogContrasts([3,2]);
+% opt.name={'Spotlight';'Position';'Interaction'};
+% opt.h={[0.005],[0.005],[0.005],[0.005],[0.005],[0.005]};
+opt.onetail='>';
+% mccopt.methods='Randomfield';
+mccopt.methods='bootstrap';
+mccopt.bootgroup={'groupST'};
+% mccopt.methods='FDR';
+mccopt.nboot=1000;
+% mccopt.permute=1;
+mccopt.bootopt=1;
+% mccopt.sigma=smoothingpic*scale;
+mccopt.tfce=0;
+
+% perform contrast
+[StatMap]=imapLMMcontrast(LMMmap,opt);
+[StatMap_c]=imapLMMmcc(StatMap,LMMmap,mccopt,fixmapMatST);
+
+% output figure;
+imapLMMdisplay(StatMap_c,1,[],'parula')
+%% bootCI for fixed effect beta
+% size(StatMap_c.betaCI)
+% size(StatMap_c.resampMat.resBeta)
+cmax=max(StatMap_c.betaCI(:));
+cmin=min(StatMap_c.betaCI(:));
+alpha=.05;
+[nboot,Nc,xSize,ySize]=size(StatMap_c.resampMat.resBeta);
+
+for ic=1:Nc
+    betaORI=squeeze(StatMap_c.beta(ic,:,:));
+    figure('Numbertitle','off','Name',StatMap_c.label{ic});
+    betaCI1=squeeze(StatMap_c.betaCI(ic,1,:,:));
+    betaCI2=squeeze(StatMap_c.betaCI(ic,2,:,:));
+    subplot(2,3,1)
+    imagesc(betaCI1,[cmin cmax]);axis('equal','off')
+    title('lower CI')
+    subplot(2,3,4)
+    imagesc(betaCI2,[cmin cmax]);axis('equal','off')
+    title('upper CI')
+    % compute Bootstrap CI
+    betatmp=squeeze(StatMap_c.resampMat.resBeta(:,ic,:,:));
+    betasort=sort(betatmp(:,:),1);
+    betaCI1b=reshape(betasort(nboot*alpha,:),[xSize,ySize])+betaORI;
+    betaCI2b=reshape(betasort(nboot*(1-alpha),:),[xSize,ySize])+betaORI;
+    subplot(2,3,2)
+    imagesc(betaCI1b,[cmin cmax]);axis('equal','off')
+    title('lower CI (bs)')
+    subplot(2,3,5)
+    imagesc(betaCI2b,[cmin cmax]);axis('equal','off')
+    title('upper CI (bs)')
+
+    subplot(2,3,3)
+    imagesc(betaCI1b-betaCI1);colorbar;axis('equal','off')
+    title('diff CI (bs-pm)')
+    subplot(2,3,6)
+    imagesc(betaCI2b-betaCI2);colorbar;axis('equal','off')
+    title('diff CI (bs-pm)')
+end
