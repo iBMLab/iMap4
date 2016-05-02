c8bd9a3d51d1db62bb9f91ae97834166922d598a
Junpeng Lao <junpeng.lao@unifr.ch>
2016-05-02 08:59:49 +0200

    similarity matrix

--- imap_utilities/rdmfixmap.m
+++ imap_utilities/rdmfixmap.m
@@ -1,2 +1,23 @@
-function [RDM, pMat] = rdmfixmap(FixMap, CondiVec, SbjVec)
+function [RDM, stRDM, pMat, pstMat] = rdmfixmap(FixMap, Mask, CondiVec, SbjVec)
+% compute representational dissimilarity matrix of smoothed fixation map
+% basic on Mahalanobis distance.
+DS         = cat(2,dummyvar(CondiVec),dummyvar(SbjVec));
+[ds,ia,ic] = unique(DS,'rows');
+unicd      = CondiVec(ia);
 
+if size(ds,1)<size(DS,1) % compute the conditonal mean for each subject
+    fixmap = zeros(size(ds,1), sum(Mask(:) == 1));
+    for ist = 1:length(ia)
+        idx           = ic==ist;
+        fixmap(ist,:) = nanmean(FixMap(idx,Mask),1);
+    end
+else
+    fixmap = FixMap(ia,Mask);
+end
+
+fixmap(isnan(fixmap))=0;
+
+
+y     = pdist(fixmap,'mahalanobis');
+stRDM = squareform(y);
+end
